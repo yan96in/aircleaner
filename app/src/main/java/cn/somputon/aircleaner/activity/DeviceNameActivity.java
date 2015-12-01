efc38d201aca482f42412659d04129d582fe8459
yan96in <yan96in@163.com>
2015-12-01 16:53:55 +0800

    first commit

---
+++ app/src/main/java/cn/somputon/aircleaner/activity/DeviceNameActivity.java
@@ -0,0 +1,15 @@
+package cn.somputon.aircleaner.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cn.somputon.aircleaner.R;
+
+public class DeviceNameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_device_name);
+    }
+}
---
+++ app/src/main/java/cn/somputon/aircleaner/fragment/SceneFragment.java
@@ -0,0 +1,31 @@
+package cn.somputon.aircleaner.fragment;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import cn.somputon.aircleaner.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SceneFragment extends Fragment {
+
+
+    public SceneFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_scene, container, false);
+    }
+
+
+}
