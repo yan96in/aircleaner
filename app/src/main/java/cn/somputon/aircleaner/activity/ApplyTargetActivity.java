43d9e49fd43c1100d4aa72a89cc45b6cb60fcba5
yan96in <yan96in@163.com>
2015-12-02 10:22:36 +0800

    save

--- app/src/main/java/cn/somputon/aircleaner/activity/ApplyTargetActivity.java
+++ app/src/main/java/cn/somputon/aircleaner/activity/ApplyTargetActivity.java
@@ -10,6 +10,6 @@ public class ApplyTargetActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_apply_target);
+        setContentView(R.layout.activity_use_target);
     }
 }
