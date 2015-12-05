e41b81a40720acd65a1ad4789aca365726d0c856
yan96in <yan96in@163.com>
2015-12-05 17:15:33 +0800

    save

--- app/src/main/java/cn/somputon/aircleaner/activity/MainActivity.java
+++ app/src/main/java/cn/somputon/aircleaner/activity/MainActivity.java
@@ -1,23 +1,35 @@
 package cn.somputon.aircleaner.activity;
 
+
+
 import android.content.res.Configuration;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v4.widget.ViewDragHelper;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import cn.somputon.aircleaner.R;
+import cn.somputon.aircleaner.fragment.MeFragment;
+import cn.somputon.aircleaner.fragment.RankFragment;
+import cn.somputon.aircleaner.fragment.SceneFragment;
+import cn.somputon.aircleaner.fragment.WeatherFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements RadioGroup.OnCheckedChangeListener{
     private DrawerLayout drawerLayout;
     private View mainContent;
     private View drawerContent;
     private ActionBarDrawerToggle toggle;
-
+    //private RadioGroup radioGroup;
+    private RadioButton rbWeater,rbScene,rbRank,rbMe;
+    private FragmentManager fragmentManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
         initView();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
+
     }
 
     private void initView() {
@@ -55,6 +68,11 @@ public class MainActivity extends AppCompatActivity {
             }
         };
         drawerLayout.setDrawerListener(toggle);
+
+
+        fragmentManager=getSupportFragmentManager();
+       // radioGroup= ((RadioGroup) findViewById(R.id.rg_bottom));
+        ((RadioGroup) findViewById(R.id.rg_bottom)).setOnCheckedChangeListener(this);
     }
 
     @Override
@@ -80,5 +98,31 @@ public class MainActivity extends AppCompatActivity {
             });
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onCheckedChanged(RadioGroup group, int checkedId) {
+        switch (checkedId){
+            case R.id.rb_weather:
+                changeFragment(new WeatherFragment());
+                break;
+            case R.id.rb_scene:
+                changeFragment(new SceneFragment());
+                break;
+            case R.id.rb_rank:
+                changeFragment(new RankFragment());
+                break;
+            case R.id.rb_me:
+                changeFragment(new MeFragment());
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void changeFragment(Fragment fragment){
+        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.main_content,fragment);
+        fragmentTransaction.commit();
     }
 }
--- app/src/main/java/cn/somputon/aircleaner/fragment/RankFragment.java
+++ app/src/main/java/cn/somputon/aircleaner/fragment/RankFragment.java
@@ -2,7 +2,7 @@ package cn.somputon.aircleaner.fragment;
 
 
 import android.os.Bundle;
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,16 +15,11 @@ import cn.somputon.aircleaner.R;
 public class RankFragment extends Fragment {
 
 
-    public RankFragment() {
-        // Required empty public constructor
-    }
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_rank, container, false);
+           View view=inflater.inflate(R.layout.fragment_rank,null);
+        return view;
     }
 
 
