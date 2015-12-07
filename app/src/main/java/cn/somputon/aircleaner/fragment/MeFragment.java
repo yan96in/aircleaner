b2833aafae19811040eaa5bfaf98992e69ca45d8
yan96in <yan96in@163.com>
2015-12-07 10:03:39 +0800

    save

--- app/src/main/java/cn/somputon/aircleaner/fragment/MeFragment.java
+++ app/src/main/java/cn/somputon/aircleaner/fragment/MeFragment.java
@@ -7,26 +7,40 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import cn.somputon.aircleaner.R;
+import cn.somputon.aircleaner.activity.MainActivity;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MeFragment extends Fragment {
-
-
-    public MeFragment() {
-        // Required empty public constructor
-    }
-
+public class MeFragment extends Fragment implements View.OnClickListener{
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_me, container, false);
+        return inflater.inflate(R.layout.fragment_me, null);
+
+    }
+    // TODO: 2015/12/5 设置监听
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
     }
 
+    public void meirirenwu(){
+        Toast.makeText(getActivity(),"tmep test",Toast.LENGTH_SHORT).show();
+    }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.meirirenwu:
+                meirirenwu();
+                break;
+            default:break;
+        }
+    }
 }
