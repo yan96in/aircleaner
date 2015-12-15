4387d95acafd5fcd82ba5bfa295b89391fcb5d14
yan96in <yan96in@163.com>
2015-12-15 09:08:05 +0800

    修改msg图标

---
+++ app/src/main/java/cn/somputon/aircleaner/view/TempView.java
@@ -0,0 +1,22 @@
+package cn.somputon.aircleaner.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by 002 on 2015/12/14.
+ */
+public class TempView extends View {
+    public TempView(Context context) {
+        super(context);
+    }
+
+    public TempView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public TempView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
