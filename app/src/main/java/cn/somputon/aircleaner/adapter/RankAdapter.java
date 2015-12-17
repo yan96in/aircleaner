a9112c2c21b7775aca5e27a3f70f9761aeb3037b
yan96in <yan96in@163.com>
2015-12-17 14:36:58 +0800

    根据给的数据分颜色显示污染等级

--- app/src/main/java/cn/somputon/aircleaner/adapter/RankAdapter.java
+++ app/src/main/java/cn/somputon/aircleaner/adapter/RankAdapter.java
@@ -1,6 +1,8 @@
 package cn.somputon.aircleaner.adapter;
 
 import android.content.Context;
+import android.content.res.ColorStateList;
+import android.support.v4.view.ViewCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,6 +23,8 @@ public class RankAdapter extends BaseAdapter {
     Button ditailNum;
     List<City> cities;
     Context context;
+    ColorStateList colorList;
+    //public  int count=5;
 
     public RankAdapter(List<City> cities, Context context) {
         this.cities = cities;
@@ -44,10 +48,9 @@ public class RankAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        City city=cities.get(position);
+        City city = cities.get(position);
         if (convertView == null) {
             convertView = LayoutInflater.from(context).inflate(R.layout.item_rank_list, null);
-
         }
         rank = ViewHolder.get(convertView, R.id.rank);
         cityName = ViewHolder.get(convertView, R.id.city);
@@ -57,6 +60,23 @@ public class RankAdapter extends BaseAdapter {
         cityName.setText(city.getCity());
         province.setText(city.getProvince());
         ditailNum.setText(city.getDetailNum());
+        if(Integer.valueOf(city.getDetailNum())>=450){
+            colorList=ColorStateList.valueOf(convertView.getResources().getColor(R.color.red_1));
+        }else if(Integer.valueOf(city.getDetailNum())>=350){
+            colorList=ColorStateList.valueOf(convertView.getResources().getColor(R.color.purple_2));
+        }else if(Integer.valueOf(city.getDetailNum())>=250){
+            colorList=ColorStateList.valueOf(convertView.getResources().getColor(R.color.orange_3));
+        }else if(Integer.valueOf(city.getDetailNum())>=200){
+            colorList=ColorStateList.valueOf(convertView.getResources().getColor(R.color.green));
+        }else if(Integer.valueOf(city.getDetailNum())>=100){
+            colorList=ColorStateList.valueOf(convertView.getResources().getColor(R.color.green));
+        }
+        //ditailNum.setBackgroundTintList(colorList);//Wrong!必须使用ViewCompat
+        ViewCompat.setBackgroundTintList(ditailNum,colorList);
+
         return convertView;
     }
+
+
 }
+
--- app/src/main/java/cn/somputon/aircleaner/fragment/RankFragment.java
+++ app/src/main/java/cn/somputon/aircleaner/fragment/RankFragment.java
@@ -1,6 +1,5 @@
 package cn.somputon.aircleaner.fragment;
 
-
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.Fragment;
@@ -8,8 +7,11 @@ import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import org.xutils.common.util.LogUtil;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,10 +30,26 @@ public class RankFragment extends Fragment {
     List<City> cityList;
     RankAdapter adapter;
     SwipeRefreshLayout srl;
+    private int lastItem;
+
     public void initData() {
         cityList = new ArrayList<>();
         city = new City("1", "石家庄", "河北", "425");
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < 5; i++) {
+            cityList.add(city);
+
+        }
+        city = new City("2", "杭州", "浙江", "325");
+        for (int i = 0; i < 5; i++) {
+            cityList.add(city);
+
+        }
+        city = new City("3", "苏州", "江苏", "225");
+        for (int i = 0; i < 5; i++) {
+            cityList.add(city);
+        }
+        city = new City("4", "西安", "陕西", "125");
+        for (int i = 0; i < 5; i++) {
             cityList.add(city);
         }
     }
@@ -43,7 +61,7 @@ public class RankFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_rank, null);
         title = ((TextView) view.findViewById(R.id.tv_title));
         title.setText("排名");
-        srl=((SwipeRefreshLayout) view.findViewById(R.id.refresh_layout));
+        srl = ((SwipeRefreshLayout) view.findViewById(R.id.refresh_layout));
         srl.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
@@ -64,6 +82,39 @@ public class RankFragment extends Fragment {
         lvRank = ((ListView) view.findViewById(R.id.lv_rank));
         adapter = new RankAdapter(cityList, getActivity());
         lvRank.setAdapter(adapter);
+        //todo 分页显示，上拉加载
+//        lvRank.setOnScrollListener(new AbsListView.OnScrollListener() {
+//            @Override
+//            public void onScrollStateChanged(AbsListView view, final int scrollState) {
+//                new Handler().postDelayed(new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        if (lastItem == adapter.getCount() && scrollState == AbsListView.OnScrollListener.SCROLL_STATE_IDLE) {
+//                            LogUtil.i("ScrollStateChanged>>>state:" + scrollState + "lastItem:" + lastItem);
+//                            if (adapter.getCount() <6) {
+//                                adapter.count+=5;
+//                                adapter.notifyDataSetChanged();
+//                            }
+//                        }
+//                    }
+//                },1500);
+//
+//            }
+//
+//            @Override
+//            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+//                lastItem = firstVisibleItem + visibleItemCount - 1;
+//                if (adapter.getCount() < 6) {
+//                    if (firstVisibleItem + visibleItemCount == totalItemCount) {
+//                        adapter.count+=5;
+//                        adapter.notifyDataSetChanged();
+//                        lvRank.setSelection(lastItem);
+//                    } else {
+//                        LogUtil.d("没有更多数据可供加载");
+//                    }
+//                }
+//            }
+//        });
         return view;
     }
 
